d09c1e5d319b290c7639e41e6623059eeb91f1bb
spacecityjams <62345526+spacecityjams@users.noreply.github.com>
2020-05-29 12:05:36 -0400

    final commit

--- wordApp.js
+++ wordApp.js
@@ -2,17 +2,17 @@ const fs = require("fs");
 
 const writestream = fs.createWriteStream("ordered-words.txt");
 
-fs.readFile('./dictionary/test-words.txt', function(err, data) {
+fs.readFile('./dictionary/words.txt', function(err, data) {
     if (err) {
         console.log("You done goofed.");
     }
-    const wordArray = data.toString().toLowerCase().split('\n');
+    const wordArray = data.toString().toLowerCase().split("\n");
+    console.log(wordArray.length);
 
     for (const word of wordArray) {
-        reorder = word.split("").sort();
-        
+        reorder = word.split("").sort();   
         if (reorder.join("") === word) {
-            console.log(word)
+            console.log(word);
             writestream.write(word + '\n');
         }       
     }    
